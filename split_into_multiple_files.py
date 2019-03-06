92006434552ffed79ff458410c39ea95fe663002
Rohola <hilbert.cantor@gmail.com>
2019-03-06 15:00:35 -0700

    modify split to multiple files

--- split_into_multiple_files.py
+++ split_into_multiple_files.py
@@ -2,13 +2,13 @@
 
 
 
-num_texts = 0
-with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
-    for line in file_reader:
-        if not line.rstrip():
-            num_texts +=1
+# num_texts = 0
+# with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
+#     for line in file_reader:
+#         if not line.rstrip():
+#             num_texts +=1
 
-print(num_texts)
+num_texts = 3956334
 
 chunk_size = num_texts//24
 
@@ -16,8 +16,8 @@ chunk_num = 0
 num_texts = 0
 with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
     for line in file_reader:
-        if not line:
+        if not line.rstrip():
             num_texts += 1
         chunk_num = num_texts//chunk_size
-        with open("chunk_"+str(chunk_num), 'w') as file_writer:
+        with open("/media/data/rohola_data/chunk_"+str(chunk_num)+".txt", 'w') as file_writer:
             file_writer.write(line)
