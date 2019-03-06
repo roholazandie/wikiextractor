150e470370d46f8685e88a2ba1ab975fa67dd801
Rohola <hilbert.cantor@gmail.com>
2019-03-06 10:27:50 -0700

    added split_into_multiple_files

---
+++ split_into_multiple_files.py
@@ -0,0 +1,23 @@
+
+
+
+
+num_texts = 0
+with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
+    for line in file_reader:
+        if not line:
+            num_texts +=1
+
+print(num_texts)
+
+chunk_size = num_texts//24
+
+chunk_num = 0
+num_texts = 0
+with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
+    for line in file_reader:
+        if not line:
+            num_texts += 1
+        chunk_num = num_texts//chunk_size
+        with open("chunk_"+str(chunk_num), 'w') as file_writer:
+            file_writer.write(line)
