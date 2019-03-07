a94537662f9c2d237e99268e0704014d746f94a3
Rohola <hilbert.cantor@gmail.com>
2019-03-07 11:41:44 -0700

    append to file

--- split_into_multiple_files.py
+++ split_into_multiple_files.py
@@ -19,5 +19,5 @@ with open("/media/data/rohola_data/cleaned_wiki.txt") as file_reader:
         if not line.rstrip():
             num_texts += 1
         chunk_num = num_texts//chunk_size
-        with open("/media/data/rohola_data/chunk_"+str(chunk_num)+".txt", 'w') as file_writer:
+        with open("/media/data/rohola_data/chunk_"+str(chunk_num)+".txt", 'a') as file_writer:
             file_writer.write(line)
